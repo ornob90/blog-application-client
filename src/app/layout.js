b782566200ff0a65211b33f7deb1390f0ba8d8d8
ornob90 <oornob49@gmail.com>
2023-12-28 23:16:24 +0600

    creating responsive navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
-      <body className={inter.className}>
+      <body className={inter.className + " max-w-[1400px] mx-auto"}>
         <Navbar />
 
         {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        secondary: "#FF3B1D",
+        primary: "#191919",
+      },
     },
   },
   plugins: [daisyui],
