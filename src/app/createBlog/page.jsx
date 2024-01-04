30adaa399424e6f401834a9b0765abec3795b91d
ornob90 <oornob49@gmail.com>
2024-01-04 07:15:56 +0600

    setting template for metadata

--- src/app/createBlog/page.jsx
+++ src/app/createBlog/page.jsx
@@ -3,6 +3,10 @@ import Container from "@/components/shared/Container/Container";
 import SectionHeader from "@/components/shared/SectionHeader/SectionHeader";
 import React from "react";
 
+export const metadata = {
+  title: "Create Blog",
+};
+
 const CreateBlog = () => {
   return (
     <Container className="mt-16 w-[85%] sm:w-[70%] md:w-[60%]">
